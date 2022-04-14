3dbf61f6b0b2c880abc8753450305641a9e69460
Antonio Pires <tonan@outlook.com.br>
2022-04-14 19:58:59 -0300

    Gif Finder App

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:gif_finder/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: const HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white,
+      primaryColor: Colors.white,
+      inputDecorationTheme: const InputDecorationTheme(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    ),
+    debugShowCheckedModeBanner: false,
+  ));
+}
